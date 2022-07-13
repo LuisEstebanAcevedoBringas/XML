0bad60c889bf73fd8db008ef1adc9edd6c67ec8a
Agus Castillo <agu13xd@hotmail.com>
2022-07-13 18:53:21 -0500

    update

--- Corregir_XML.py
+++ Corregir_XML.py
@@ -2,14 +2,16 @@ import xml.etree.ElementTree as ET
 from glob import glob
 import os
 
-label_change = {1:0, 3:1, 4:2, 5:3, 6:4}
-name_change = {0:'cloth', 1:'none', 2:'respirator', 3:'surgical', 4:'valve'}
+label_change = {1: 0, 3: 1, 4: 2, 5: 3, 6: 4}
+name_change = {0: 'cloth', 1: 'none',
+               2: 'respirator', 3: 'surgical', 4: 'valve'}
+
 
 def CorregirXLM(path):
     tree = ET.parse(path)
     root = tree.getroot()
 
-    #Cambiamos el valor del tag "folder" de "train" a "valid"
+    # Cambiamos el valor del tag "folder" de "train" a "valid"
     new_folder = root.find("folder")
     new_folder.text = "valid"
 
@@ -19,51 +21,55 @@ def CorregirXLM(path):
     Contador_objetos = 0
 
     for object in root.iter("object"):
-        object.remove(object.find("score")) #Eliminamos el tag "score" de cada objeto
+        # Eliminamos el tag "score" de cada objeto
+        object.remove(object.find("score"))
         new_label = object.find("label")
         new_name = object.find("name")
 
-        #Cambiamos el tag label y name de cada objeto.
+        # Cambiamos el tag label y name de cada objeto.
         last_label = int(new_label.text)
-        new_label.text = label_change[last_label]
-        new_name.text = name_change[new_label.text]
+        new_label.text = str(label_change[last_label])
+        new_name.text = str(name_change[int(new_label.text)])
 
         bbox = object.find('bndbox')
-        #bbox.remove(bbox.find("object_prob"))
+        # bbox.remove(bbox.find("object_prob"))
         xmin = int(bbox.find('xmin').text) - 1
         ymin = int(bbox.find('ymin').text) - 1
         xmax = int(bbox.find('xmax').text) - 1
         ymax = int(bbox.find('ymax').text) - 1
 
-        Area_pix = (xmax - xmin) * (ymax - ymin) #Calculamos el area de cada bndbox
+        # Calculamos el area de cada bndbox
+        Area_pix = (xmax - xmin) * (ymax - ymin)
 
-        #Agregamos el tag "obj_size" a cada bndbox
+        # Agregamos el tag "obj_size" a cada bndbox
         object_size = bbox.find("obj_size")
         object_size.text = str(Area_pix)
 
-        #Agregamos el tag "size" a cada objeto
+        # Agregamos el tag "size" a cada objeto
         add_size = ET.SubElement(object, "size")
-        if Area_pix < 3666 :
+        if Area_pix < 3666:
             add_size.text = "0"
         elif Area_pix > 3665 and Area_pix < 14522:
             add_size.text = "1"
         elif Area_pix > 14521:
             add_size.text = "2"
-    
+
         Contador_objetos += 1
 
-    #Actualizamos el numero del tag "faces" segun la cantidad de "objetos en el xml
+    # Actualizamos el numero del tag "faces" segun la cantidad de "objetos en el xml
     new_face = root.find("faces")
     new_face.text = str(Contador_objetos)
 
-    #Guardamos el archivo modificado en el nuevo directorio
+    # Guardamos el archivo modificado en el nuevo directorio
     file_Content = ET.tostring(root, encoding='unicode')
     new_File = open(path, "w")
-    new_File.write(file_Content) 
+    new_File.write(file_Content)
+
 
 if __name__ == "__main__":
 
     #Paths = glob("./valid/Annotations/*.xml")
-    #for p in Paths:
+    # for p in Paths:
     #    CorregirXLM(p)
-    CorregirXLM("./E_0AcW4VgAYXanL.xml")
+    CorregirXLM(
+        "/Users/agustincastillo/Downloads/valid/Annotations/E_0AcW4VgAYXanL.xml")
